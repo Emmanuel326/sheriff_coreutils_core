d3cd21179bf62e672bee858688dd0d5011c1b3ed
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-10-07 21:30:13 +0300

    I completed tha cp [copy] commnd and now ut ahndles erros gracefully

--- src/cp.c
+++ src/cp.c
@@ -25,6 +25,38 @@ int main(int argc, char *argv[])
 	}
 
 	//3.Open the destination file for writing
+	//"w" mode creates the file if it doesnt exist or truncates it if it does
+	dest_fp = fopen(argv[2], "w");
+	if(dest_fp = NULL)
+	{
+		perror("Error opening destination file");
+		fclose(source_fp);//close the source file before closing
+		exit(EXIT_FAILURE);
+	}
+
+	//CORE COPY LOOP METHOD(). it copies cha-by-char
+	while((c = fgetc(source_fp)) != EOF)
+	{
+		//read char from source and write in dest
+		if(fputc(c, dest_fp) == EOF)
+		{
+			perror("Error writing to destination file");
+			break; //exit the loop on write error
+		}
+	}
+
+	//5. close files and check for I/O errors
+	//the loop may have terminated due to EOF or a read error 
+	if(ferror(source_fp))
+	{
+		fprintf(stderr, "Read error occurred on source file.\n");
+	}
+
+	//always close file streams
+	fclose(source_fp);
+	fclose(dest_fp);
+
+	return 0;
 
 
 }
